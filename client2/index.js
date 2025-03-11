9560ca946480f30e1c1cbce5e9057693c502e19d
Juan Silva <juanse909@hotmail.com>
2025-03-11 18:43:54 -0500

    feat: user and post GET method added and working

--- client2/index.js
+++ client2/index.js
@@ -1,8 +1,55 @@
-document.getElementById("get-btn").addEventListener("click", getUsers);
-
+// GET user from database
 function getUsers() {
   fetch("http://localhost:5050/users")
     .then((response) => response.json())
-    .then((data) => console.log("get response", data))
+    .then((data) => {
+      console.log("get response user data", data);
+
+      const container = document.getElementById("users-generate-container");
+
+      container.innerHTML = "";
+
+      data.forEach((e) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `
+            <h2>${e.userName}</h2>
+            <p>${e.userEmail}</p>
+        `;
+        container.appendChild(card);
+      });
+    })
     .catch((error) => console.error("Error:", error));
 }
+document
+  .getElementById("users-generate-button")
+  .addEventListener("click", getUsers);
+
+// GET post from database
+const getPosts = () => {
+  fetch("http://localhost:5050/posts")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("get response post data", data);
+
+      const container = document.getElementById("posts-generate-container");
+
+      container.innerHTML = "";
+
+      data.forEach((e) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `
+            <img src="${e.postImg}" alt="${e.postTitle}">
+            <h2>${e.postTitle}</h2>
+            <p>${e.postContent}</p>
+        `;
+        container.appendChild(card);
+      });
+    })
+    .catch((error) => console.error("Error:", error));
+};
+
+document
+  .getElementById("posts-generate-button")
+  .addEventListener("click", getPosts);
